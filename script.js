8aafcac4c71f09b58c2b24a907e70e506337d2b8
Yair Sanso <ygsanso@gmail.com>
2025-02-05 12:01:49 -0300

    agrego la configuración de firebase para probar si funciona

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Configuración de Firebase
-const firebaseConfig = {
-    apiKey: "TU_API_KEY",
-    authDomain: "TU_AUTH_DOMAIN",
-    projectId: "TU_PROJECT_ID",
-    storageBucket: "TU_STORAGE_BUCKET",
-    messagingSenderId: "TU_MESSAGING_SENDER_ID",
-    appId: "TU_APP_ID"
-  };
+// const firebaseConfig = {
+//  apiKey: "TU_API_KEY",
+//  authDomain: "TU_AUTH_DOMAIN",
+//  projectId: "TU_PROJECT_ID",
+//  storageBucket: "TU_STORAGE_BUCKET",
+//  messagingSenderId: "TU_MESSAGING_SENDER_ID",
+//  appId: "TU_APP_ID"
+// };
   
   // Inicializar Firebase
-  const app = firebase.initializeApp(firebaseConfig);
-  const db = firebase.firestore();
+  //const app = firebase.initializeApp(firebaseConfig);
+  //const db = firebase.firestore(); 
   
   // Referencias al DOM
   const formulario = document.getElementById('formulario');
